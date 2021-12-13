58d3a70a1931685219ac313e80278e04a3625216
N-akademy <56726632+N-akademy@users.noreply.github.com>
2021-12-13 16:14:04 +0100

    Atelier POO3

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<?php
+
+require '../vendor/autoload.php';
+
+/** ✅ DEBUT DE LA ZONE À MODIFIER ✅ **/
+
+use App\Shield;
+use App\Weapon;
+use App\Fighter;
+
+$heracles = new Fighter('Heracles', 20, 6, 'heracles.svg');
+$bird1 = new Fighter('Bird', 25, 12, 'bird.svg');
+$bird2 = new Fighter('Bird', 25, 12, 'bird.svg');
+$bird3 = new Fighter('Bird', 25, 12, 'bird.svg');
+
+
+$sword = new Weapon();
+$heracles->setWeapon($sword);
+
+$shield = new Shield();
+$heracles->setShield($shield);
+
+
+/** FIN DE LA ZONE A MODIFIER **/
+/** ⛔ Ne pas modifier en dessous ⛔ **/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heracles Labours</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Heracles vs Stymphalian Birds</h1>
+    </header>
+    <main>
+        <div class="fighters">
+            <a href="#hero">
+                <figure class="heracles">
+                    <img src="<?= $heracles->getImage() ?>" alt="heracles">
+                    <figcaption><?= $heracles->getName() ?></figcaption>
+                </figure>
+            </a>
+        </div>
+
+        <?php include 'map.php' ?>
+
+    </main>
+
+    <?php include 'inventory.php' ?>
+</body>
+
+</html>
---
+++ src/Fighter.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App;
+
+use App\Shield;
+use App\Weapon;
+
+class Fighter
+{
+    public const MAX_LIFE = 100;
+
+    private string $name;
+
+    private int $strength;
+    private int $dexterity;
+    private string $image = 'fighter.svg';
+
+    private int $life = self::MAX_LIFE;
+
+    private ?Weapon $weapon = null;
+    private ?Shield $shield = null;
+
+    public function __construct(
+        string $name,
+        int $strength = 10,
+        int $dexterity = 5,
+        string $image = 'fighter.svg'
+    ) {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+        $this->image = $image;
+    }
+
+    
+    public function getDamage(): int
+    {
+        $damage = $this->getStrength();
+        if($this->getWeapon() !== null) {
+            $damage += $this->getWeapon()->getDamage();
+        }
+        return $damage;
+    }
+
+    public function getDefense(): int
+    {
+        $defense = $this->getDexterity();
+        if ($this->getShield() !== null) {
+            $defense += $this->getShield()->getProtection();
+        }    
+
+        return $defense;
+    }
+
+     /**
+     * Get the value of weapon
+     */ 
+    public function getWeapon(): ?Weapon
+    {
+        return $this->weapon;
+    }
+
+    /**
+     * Set the value of weapon
+     *
+     */ 
+    public function setWeapon(Weapon $weapon): void
+    {
+        $this->weapon = $weapon;
+    }
+
+    /**
+     * Get the value of shield
+     */ 
+    public function getShield(): ?Shield
+    {
+        return $this->shield;
+    }
+
+    /**
+     * Set the value of shield
+     *
+     */ 
+    public function setShield(?Shield $shield): void
+    {
+        $this->shield = $shield;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of image
+     */
+    public function getImage(): string
+    {
+        return 'assets/images/' . $this->image;
+    }
+
+
+    public function fight(Fighter $adversary): void
+    {
+        $damage = rand(1, $this->getDamage()) - $adversary->getDefense();
+        if ($damage < 0) {
+            $damage = 0;
+        }
+        $adversary->setLife($adversary->getLife() - $damage);
+    }
+
+    /**
+     * Get the value of life
+     */
+    public function getLife(): int
+    {
+        return $this->life;
+    }
+
+    /**
+     * Set the value of life
+     *
+     */
+    public function setLife(int $life)
+    {
+        if ($life < 0) {
+            $life = 0;
+        }
+        $this->life = $life;
+    }
+
+    public function isAlive(): bool
+    {
+        return $this->getLife() > 0;
+    }
+
+    /**
+     * Get the value of strength
+     */
+    public function getStrength(): int
+    {
+        return $this->strength;
+    }
+
+    /**
+     * Set the value of strength
+     *
+     */
+    public function setStrength($strength): void
+    {
+        $this->strength = $strength;
+    }
+
+    /**
+     * Get the value of dexterity
+     */
+    public function getDexterity(): int
+    {
+        return $this->dexterity;
+    }
+
+    /**
+     * Set the value of dexterity
+     *
+     */
+    public function setDexterity($dexterity): void
+    {
+        $this->dexterity = $dexterity;
+    }
+}
